c2f3ab50a13d36a1d957f964dfc5c1e00febf123
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2023-04-29 22:20:00 +0800

    Initial Commit

---
+++ Mob/MobApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobApp.swift
+//  Mob
+//
+//  Created by 朱浩宇 on 2023/4/29.
+//
+
+import SwiftUI
+
+@main
+struct MobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
